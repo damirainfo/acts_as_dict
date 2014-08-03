3534e6ddd8a67bf00098427dde024dba865116cd
曾竟 <zengjing@crunii.com>
2014-08-03 14:44:05 +0800

    add migration file to app

--- lib/acts_as_dict/engine.rb
+++ lib/acts_as_dict/engine.rb
@@ -1,4 +1,10 @@
 module ActsAsDict
   class Engine < ::Rails::Engine
+    initializer "acts as dict load_app_instance_data" do |app|
+      app.class.configure do
+        #Pull in all the migrations from Dict to the application
+        config.paths['db/migrate'].concat ActsAsDict::Engine.paths['db/migrate'].existent
+      end
+    end
   end
 end
