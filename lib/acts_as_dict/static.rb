ab8b9a9cb31c9d06a75fc2323bc66fcad3022e9b
曾竟 <zengjing@crunii.com>
2014-08-07 19:20:20 +0800

    zengjing act as dict fix

--- lib/acts_as_dict/static.rb
+++ lib/acts_as_dict/static.rb
@@ -1,14 +1,32 @@
 module ActsAsDict
   module Static
-    def acts_as_dict_attr(attr)
-      attr.each {|k, v|
+    def acts_as_dict(*args)
+      options = args.extract_options!.dup
+
+      args.map(&:to_sym).uniq.each { |k|
+        class_eval %{
+          def #{k}_human
+            ActsAsDict::StaticValue.where(static_type_code: "#{k}", code: #{k}).first.name
+          end
+        }
+      }
+
+      args.map(&:to_sym).uniq.each {|k|
+        instance_eval %{
+          def #{k.to_s.pluralize}
+            ActsAsDict::StaticValue.#{k}
+          end
+        }
+      }
+
+      options.each {|k, v|
         class_eval %{
           def #{k}_human
             ActsAsDict::StaticValue.where(static_type_code: "#{v}", code: #{k}).first.name
           end
         }
       }
-      attr.each {|k, v|
+      options.each {|k, v|
         instance_eval %{
           def #{k.to_s.pluralize}
             ActsAsDict::StaticValue.#{v}
